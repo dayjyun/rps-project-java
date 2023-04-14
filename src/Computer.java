0fddb60dd7cbcbb24ca92666f0c6594773208e27
dayjyun <kbarriosdev@gmail.com>
2023-04-14 01:32:28 -0500

    completed doc strings

--- src/Computer.java
+++ src/Computer.java
@@ -1,5 +1,10 @@
 import java.util.Random;
 
+/*
+The Computer is in charge of creating a virtual playing experience if there is no other live-player available. It inherits the properties found in the parent to keep code
+modular. The method it overwrites is the computerMove which is set to randomly select a value to use against playerOne.
+ */
+
 public class Computer extends Player implements ComputerMove {
     public Computer(String name, int points) {
         super(name, points);
--- src/GameAlgorithm.java
+++ src/GameAlgorithm.java
@@ -1,6 +1,15 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+/*
+The GameAlgorithm class is in charge of determining whether the player would like to play against the in-game AI, or with another player. The if-statement for case 2 is required
+as there is a session storage in play as all methods pass at least playerOne and playerTwo objects. The moment case 1 is selected, the entire game will persist playerTwo as the
+computer. Case 2 handles that hidden bug since it will create a new instance for a HumanPlayer and reassigns the playerTwo Computer to simply be a human.
+
+Case 3 handles the name change if the user desires to change any of the player's names. Lastly, the default case is in player to make sure the user does not enter any unwanted
+inputs and using recursion to simulate the menu remaining unchanged.
+ */
+
 public class GameAlgorithm {
     public static void choosePlayer(Player playerOne, Player playerTwo, int ties, Scanner playerOneInput) throws IOException {
         Computer cpu = new Computer("CPU", 0);
